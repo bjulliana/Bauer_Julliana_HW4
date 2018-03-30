02b28421b6bb9f67276d916a9be17c62b725b187
Julliana <bjulliana@gmx.com>
2018-03-30 12:58:30 -0400

    Final Adjustments
    Final Adjustments
    Event Listener for transitionend

--- js/main.js
+++ js/main.js
@@ -10,28 +10,35 @@
         closeLightbox = document.querySelector('.close-lightbox'),
         unsupported = document.querySelector('.unsupported'),
         video = document.querySelector('video'),
+        banner = document.querySelectorAll('#houseImages'),
         houseImages = document.querySelectorAll('#houseImages img'),
         description = document.querySelectorAll('.house-name'),
         houseInfo = document.querySelector('.house-info'),
         details = [
           {
             tagline: 'House Stark',
-            info: 'Rulers of the North from the castle of Winterfell. Recently retook their lands from House Bolton, and currently preparing for the war against the Night King. Its current head is Lady Sansa Stark, who jointly rules the North with Warden Jon Snow.',
+            info: `House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`
           },{
             tagline: 'House Baratheon',
-            info: `Rulers of the Crownlands and Stormlands from the Red Keep and Storm's End, respectively. Extinct in actuality due to all mainline members being deceased but the bloodline is still alive, technically speaking, through Robert the Usurper's many bastard children. The line legally ended with the death of King Tommen I Baratheon, the bastard son of twins Queen Cersei Lannister and Ser Jaime Lannister, who was legally the last remaining Baratheon.`,
+            info: `House Baratheon of Storm's End is a legally extinct Great House of Westeros. A cadet branch was formerly the royal house, but House Lannister now controls the throne. House Baratheon traditionally ruled the Stormlands on the eastern coast of Westeros, aptly named for its frequent storms, from their seat of Storm's End.
+            House Baratheon became the royal house of the Seven Kingdoms after Robert Baratheon led a rebellion against the Targaryen dynasty. At the end of the rebellion, Robert ascended the Iron Throne as Robert I and married Cersei Lannister after the death of Lyanna Stark.`,
           },{
             tagline: 'House Lannister',
-            info: `The current royal house of the Seven Kingdoms from the Red Keep of King's Landing in the Crownlands and rulers of the Westerlands from the castle of Casterly Rock. Its leadership is currently disputed by Queen Cersei I Lannister and Lord Tyrion Lannister.`,
+            info: `House Lannister of Casterly Rock is one of the Great Houses of Westeros, one of its richest and most powerful families and oldest dynasties. It is also the current royal house of the Seven Kingdoms following the extinction of House Baratheon of King's Landing, which had been their puppet house anyway.
+            The Lannisters rule over the Westerlands. Their seat is Casterly Rock, a massive rocky promontory overlooking the Sunset Sea which has had habitations and fortifications built into it over the millennia. They are the Lords Paramount of the Westerlands and Wardens of the West. As the new royal house, they also rule directly over the Crownlands from their seat of the Red Keep in King's Landing, the traditional seat of the royal family.`,
           },{
             tagline: 'House Tully',
-            info: `The former rulers of the Riverlands, ruling from Riverrun, until the Red Wedding. Riverrun is currently under House Lannister's control. Its current head is Edmure Tully, who remains a captive of House Lannister.`,
+            info: `House Tully of Riverrun is an exiled Great House of Westeros. Its most senior member carried the title of Lord of Riverrun and Lord Paramount of the Trident, until the Red Wedding. The current head is Lord Edmure Tully, son of the late Hoster Tully. The Tully sigil is a silver trout on a red and blue background. Their house words are "Family, Duty, Honor`,
           },{
             tagline: 'House Greyjoy',
-            info: 'Rulers of the Iron Islands from the castle of Pyke. Divided, with members supporting either the Lannister or Stark/Targaryen cause. Its leadership is currently disputed by King Euron Greyjoy and claimant Lady Yara Greyjoy.',
+            info: `House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands, a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. The head of the house is the Lord Reaper of Pyke.
+            House Greyjoy's sigil is traditionally a golden kraken on a black field. Their house words are "We Do Not Sow," although the phrase "What Is Dead May Never Die" is also closely associated with House Greyjoy and their bannermen, as they are associated with the faith of the Drowned God.`,
           },{
             tagline: 'House Arryn',
-            info: 'Rulers of the Vale of Arryn from the castle of the Eyrie. It was integrated with the Kingdom of the North until King Jon Snow abdicated. They currently hold allegiance to House Targaryen. Its current head is Lord Robin Arryn.Info',
+            info: `House Arryn of the Eyrie is one of the Great Houses of Westeros. It has ruled over the Vale of Arryn for millennia, originally as the Kings of Mountain and Vale and more recently as Lords Paramount of the Vale and Wardens of the East under the Targaryen kings and Baratheon-Lannister kings. The nominal head of House Arryn is Robin Arryn, the Lord of the Eyrie, with his stepfather Petyr Baelish acting as Lord Protector until he reaches the age of majority`,
+          },{
+            tagline: 'House Targaryen',
+            info: `House Targaryen of Dragonstone is a Great House of Westeros and was the ruling royal House of the Seven Kingdoms for three centuries since it conquered and unified the realm, before it was deposed during Robert's Rebellion and House Baratheon replaced it as the new royal House. The few surviving Targaryens fled into exile to the Free Cities of Essos across the Narrow Sea. Currently based on Dragonstone off of the eastern coast of Westeros, House Targaryen seeks to retake the Seven Kingdoms from House Lannister, who formally replaced House Baratheon as the royal House following the destruction of the Great Sept of Baelor.`,
           }
         ],
         imageBanner = document.querySelector("#houseImages"),
@@ -43,11 +50,12 @@
   function moveBanner() {
 		imageBanner.style.right = (offSet * this.dataset.offset) + "px";
 		let houseName = this.className.split(' ')[1].capIt();
-    wait(500).then(() => {
-      description.forEach(description => description.innerHTML = details[this.dataset.offset].tagline),
-      houseInfo.innerHTML = details[this.dataset.offset].info;
-  });
-    wait(1000).then(() => loadMovie(houseName));
+    let dataset = this.dataset.offset;
+    imageBanner.addEventListener('transitionend', function() {
+      description.forEach(description => description.innerHTML = (`House ${houseName}`)),
+      houseInfo.innerHTML = details[dataset].info;
+      loadMovie(houseName);
+    });
   }
   
   function loadMovie(house){
@@ -78,5 +86,6 @@
   sigils.forEach(sigil => sigil.addEventListener('click', moveBanner));
   closeLightbox.addEventListener('click', closeLBox);
   video.addEventListener('ended',closeLBox);
+
 
 })();
